8d806228886901489e1afaef93c421d6f520d2b7
mohammedfathaah <mohammedfathaah@gmail.com>
2025-10-18 22:51:41 +0530

    Oct 9 :to filter out datas with a specific conditions like gte,gt,lte,lt ,, sort datas in ascending order and sort datas in descending order,Update(to update the name or anything in a given data,Delete(to delete a specific or delete specific many datas,and atlast To delete all the datas in a db

--- work.py
+++ work.py
@@ -11,20 +11,38 @@ mydb=myclient['mydatabase']
 mycol=mydb['mycollection']
 # mydata={'name':'nihal','age':21,'course':'wordpress'}
 # x=mycol.insert_one(mydata)
-# users = [
-#     {"_id": 1, "name": "Aarav", "age": 23, "email": "aarav23@gmail.com", "city": "Kochi"},
-#     {"_id": 2, "name": "Diya", "age": 21, "email": "diya21@yahoo.com", "city": "Trivandrum"},
-#     {"_id": 3, "name": "Rahul", "age": 25, "email": "rahul25@gmail.com", "city": "Kozhikode"},
-#     {"_id": 4, "name": "Neha", "age": 22, "email": "neha22@outlook.com", "city": "Thrissur"},
-#     {"_id": 5, "name": "Vishnu", "age": 24, "email": "vishnu24@gmail.com", "city": "Aluva"},
-#     {"_id": 6, "name": "Sneha", "age": 20, "email": "sneha20@yahoo.com", "city": "Kottayam"},
-#     {"_id": 7, "name": "Aditya", "age": 23, "email": "aditya23@gmail.com", "city": "Palakkad"},
-#     {"_id": 8, "name": "Meera", "age": 26, "email": "meera26@hotmail.com", "city": "Ernakulam"},
-#     {"_id": 9, "name": "Arjun", "age": 22, "email": "arjun22@gmail.com", "city": "Kannur"},
-#     {"_id": 10, "name": "Lakshmi", "age": 24, "email": "lakshmi24@gmail.com", "city": "Alappuzha"}
-# ]
-# x=mycol.insert_many(users)
-data=mycol.find_one({'email':'lakshmi24@gmail.com'})
+users = [
+    {"name": "Aarav", "age": 23, "email": "aarav23@gmail.com", "city": "Kochi"},
+    {"name": "Diya", "age": 21, "email": "diya21@yahoo.com", "city": "Trivandrum"},
+    {"name": "Rahul", "age": 25, "email": "rahul25@gmail.com", "city": "Kozhikode"},
+    {"name": "Neha", "age": 22, "email": "neha22@outlook.com", "city": "Thrissur"},
+    {"name": "Vishnu", "age": 24, "email": "vishnu24@gmail.com", "city": "Aluva"},
+    {"name": "Sneha", "age": 20, "email": "sneha20@yahoo.com", "city": "Kottayam"},
+    {"name": "Aditya", "age": 23, "email": "aditya23@gmail.com", "city": "Palakkad"},
+    {"name": "Meera", "age": 26, "email": "meera26@hotmail.com", "city": "Ernakulam"},
+    {"name": "Arjun", "age": 22, "email": "arjun22@gmail.com", "city": "Kannur"},
+    {"name": "Lakshmi", "age": 24, "email": "lakshmi24@gmail.com", "city": "Alappuzha"}
+]
+x=mycol.insert_many(users)
+data=mycol.find()
+for i in data:
+    print(i)
+# print(data)
+print('-'*60)
+# mydata=mycol.find().sort('age',-1)
+# for i in mydata:
+#     print(i)
+# myquery={'email':'aarav23@gmail.com'}
+# newvalue={"$set":{'name':'naruto'}}
+# mycol.update_many(myquery,newvalue)
+# data=mycol.find()
 # for i in data:
 #     print(i)
-print(data)
+
+# myquery={'email':'aarav23@gmail.com'}
+# mycol.delete_many(myquery)
+# for i in mycol.find():
+#     print(i)
+
+mycol.delete_many({})
+print('document is deleted')
